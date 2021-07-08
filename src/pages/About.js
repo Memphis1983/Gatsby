ff1729f04c94e96b7c81ce30e59c8c9d96838cbc
Memphis1983 <kiran.iyer83@gmail.com>
2021-07-08 15:39:24 +0530

    little update

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from './Layout'
+
+const About = () => {
+    return (
+        
+        <Layout pageTitle="About Us">
+            <h2>Hello there, I'm the proud creator of this site.</h2>
+        </Layout>
+            
+
+    )
+}
+
+export default About;
